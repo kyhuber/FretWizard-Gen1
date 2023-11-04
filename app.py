f080e0e88daad855503b70702f42e3b74e79f743
Kyle Huber <kyhuber@gmail.com>
2023-11-04 00:55:20 -0700

    Restructure fretboard.html

--- app.py
+++ app.py
@@ -5,17 +5,27 @@ app = Flask(__name__)
 # Constants
 INSTRUMENT_STRINGS = {
     'guitar': ['E', 'B', 'G', 'D', 'A', 'E'],
+    'bass' : ['G', 'D', 'A', 'E'],
     'mandolin': ['E', 'A', 'D', 'G'],
-    'banjo': ['D', 'B', 'G', 'D', 'G']  # Assuming a 5-string banjo
+    'banjo': ['D', 'B', 'G', 'D', 'G'],  # Assuming a 5-string banjo
 }
 ALL_NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-# Utility Functions
-def get_notes_in_key(key):
-    major_scale_intervals = [2, 2, 1, 2, 2, 2, 1]
+major_scale_intervals = [2, 2, 1, 2, 2, 2, 1]
+minor_scale_intervals = [2, 1, 2, 2, 1, 2, 2]
+
+def get_notes_in_key(key, scale_type):
+    # Choose the scale intervals based on the scale type
+    if scale_type == "major":
+        scale_intervals = major_scale_intervals
+    elif scale_type == "minor":
+        scale_intervals = minor_scale_intervals
+    else:
+        raise ValueError("Invalid scale type. Choose 'major' or 'minor'.")
+    
     start_index = ALL_NOTES.index(key)
     scale_notes = [ALL_NOTES[start_index]]
-    for interval in major_scale_intervals:
+    for interval in scale_intervals:
         start_index = (start_index + interval) % 12
         scale_notes.append(ALL_NOTES[start_index])
     return scale_notes
@@ -41,10 +51,13 @@ def index():
 @app.route('/fretboard', methods=['POST'])
 def fretboard():
     tuning = request.form.getlist('tuning[]')
-    instrument = request.form.get('instrument', 'guitar')  # Default to 'guitar'
+    instrument = request.form.get('instrument', 'guitar').capitalize()  # Default to 'guitar'
     selected_key = request.form.get('key', 'C')
-    notes_in_key = get_notes_in_key(selected_key)   
+    scale_type = request.form.get('scale_type', 'major')  # Default to 'major'
+    notes_in_key = get_notes_in_key(selected_key, scale_type)   
     return render_template('fretboard.html', tuning=tuning, instrument=instrument, selected_key=selected_key, notes_in_key=notes_in_key)
+
+app.debug = True
 
 if __name__ == '__main__':
     app.run(debug=True)
