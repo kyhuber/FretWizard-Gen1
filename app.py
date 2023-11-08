62800715f5fa28ace309cd520d5871fbf088e9c1
Kyle Huber <kyhuber@gmail.com>
2023-11-08 01:21:22 -0800

    Rewrite with add string functionality

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 # Constants
 INSTRUMENT_STRINGS = {
     'guitar': ['E', 'B', 'G', 'D', 'A', 'E'],
-    'bass' : ['G', 'D', 'A', 'E'],
+    'bass': ['G', 'D', 'A', 'E'],
     'mandolin': ['E', 'A', 'D', 'G'],
     'banjo': ['D', 'B', 'G', 'D', 'G'],  # Assuming a 5-string banjo
 }
@@ -13,6 +14,13 @@ ALL_NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 major_scale_intervals = [2, 2, 1, 2, 2, 2, 1]
 minor_scale_intervals = [2, 1, 2, 2, 1, 2, 2]
+
+INSTRUMENT_IMAGES = {
+    'guitar': 'lespaul.jpg',
+    'bass': 'jazzbass.jpg',
+    'mandolin': 'mandolin.jpg',
+    'banjo': 'banjo.jpg',
+}
 
 def get_notes_in_key(key, scale_type):
     # Choose the scale intervals based on the scale type
@@ -38,26 +46,118 @@ def get_note_at_fret(string, fret):
 def utility_functions():
     return dict(get_note_at_fret=get_note_at_fret)
 
-# Routes
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    instrument = request.form.get('instrument', 'guitar')  # Default to 'guitar'
+    instrument = None
+    tuning_options = []
+
     if request.method == 'POST':
-        tuning_options = INSTRUMENT_STRINGS.get(instrument, [])
-    else:
-        tuning_options = []
+        if request.form['action'] == 'Select Instrument':
+            instrument = request.form.get('instrument').lower()
+            tuning_options = INSTRUMENT_STRINGS.get(instrument, [])
+        elif request.form['action'] == 'Confirm Tuning':
+            tuning = request.form.getlist('tuning[]')
+            instrument = request.form.get('instrument_hidden').lower()
+
+            # Check if the user has selected a tuning
+            if tuning:
+                session['instrument'] = instrument.capitalize()
+                session['tuning'] = tuning
+                return redirect(url_for('fretboard'))
+            else:
+                # Display an error message to the user
+                flash('You must select a tuning.')
+
     return render_template('index.html', instrument=instrument, tuning_options=tuning_options)
 
-@app.route('/fretboard', methods=['POST'])
-def fretboard():
-    tuning = request.form.getlist('tuning[]')
-    instrument = request.form.get('instrument', 'guitar').capitalize()  # Default to 'guitar'
-    selected_key = request.form.get('key', 'C')
-    scale_type = request.form.get('scale_type', 'major')  # Default to 'major'
-    notes_in_key = get_notes_in_key(selected_key, scale_type)   
-    return render_template('fretboard.html', tuning=tuning, instrument=instrument, selected_key=selected_key, notes_in_key=notes_in_key)
 
-app.debug = True
+def invert_tuning(tuning):
+    return tuning[::-1]
+
+@app.route('/fretboard', methods=['GET', 'POST'])
+def fretboard():
+    # Get the instrument and tuning from the session
+    instrument = session.get('instrument', 'Guitar')
+    tuning = session.get('tuning', [])
+
+    # Get the selected key and scale type from the request, if they exist
+    selected_key = request.form.get('key')
+    scale_type = request.form.get('scale_type')
+
+    # If the selected key and scale type exist, get the notes in the key
+    notes_in_key = []
+    if selected_key and scale_type:
+        notes_in_key = get_notes_in_key(selected_key, scale_type)
+
+    # Get the image for the instrument
+    instrument_image = url_for('static', filename='images/fretboard_photo.jpg')
+    if 'instrument' in session and session['instrument'].lower() in INSTRUMENT_IMAGES:
+        instrument_image = url_for('static', filename=f'images/{INSTRUMENT_IMAGES[session["instrument"].lower()]}')
+
+    # Render the fretboard form
+    return render_template('fretboard.html',
+                           tuning=tuning,
+                           inverted_tuning=invert_tuning(tuning),
+                           instrument=session.get('instrument', 'Guitar'),
+                           selected_key=selected_key,
+                           notes_in_key=notes_in_key,
+                           instrument_image=instrument_image)
+
+@app.route('/fretboard_new', methods=['GET'])
+def fretboard_new():
+    strings = 4  # Number of strings
+    default_tuning = ['G', 'D', 'A', 'E']  # Default tuning for a 4-string bass
+    # Pass the ALL_NOTES constant and default tuning to the template
+    return render_template('fretboard_new.html', all_notes=ALL_NOTES, strings=strings, default_tuning=default_tuning)
+
+
+
+@app.route('/get_scale_notes')
+def get_scale_notes():
+    key = request.args.get('key')
+    scale_type = request.args.get('scaleType', 'major')  # Default to 'major' if not provided
+    scale_notes = calculate_scale_notes(key, scale_type)
+    return jsonify(success=True, notes=scale_notes)
+
+def calculate_scale_notes(key, scale_type):
+    # Define the notes in the chromatic scale
+    chromatic_scale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+    # Find the starting index of the key in the chromatic scale
+    start_index = chromatic_scale.index(key)
+
+    # Define steps for major and minor scales
+    steps = {
+        'major': ['W', 'W', 'H', 'W', 'W', 'W', 'H'],
+        'minor': ['W', 'H', 'W', 'W', 'H', 'W', 'W']  # Natural minor scale steps
+    }
+
+    # Calculate the scale notes based on the steps for the given scale type
+    scale_notes = [key]  # Start with the tonic note
+    for step in steps[scale_type]:
+        if step == 'W':
+            start_index = (start_index + 2) % 12  # Move a whole step
+        elif step == 'H':
+            start_index = (start_index + 1) % 12  # Move a half step
+        scale_notes.append(chromatic_scale[start_index])
+
+    return scale_notes
+
+
+@app.route('/add_string', methods=['POST'])
+def add_string():
+    # Check if 'tuning' is already in session, if not, create it
+    if 'tuning' not in session:
+        session['tuning'] = ['E', 'A', 'D', 'G']  # Example default tuning
+
+    # Append a new string with a default note (e.g., 'E')
+    session['tuning'].append('E')  # You can choose a different default note if you wish
+
+    # Save the changes to the session
+    session.modified = True
+
+    # Return a success response with the updated tuning
+    return jsonify(success=True, tuning=session['tuning'])
 
 if __name__ == '__main__':
     app.run(debug=True)
